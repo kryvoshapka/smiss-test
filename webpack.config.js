9f4029cfbcd4813a8cb0462aaae1cf7c6a9c5be7
Tetiana <tanya1krita@gmail.com>
2018-06-23 17:45:19 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        rules: [
+            {test: /\.(js|jsx)$/},
+            {exclude: /(node_modules|bower_components)/},
+            {loader: 'babel-loader'},
+            {query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+};
